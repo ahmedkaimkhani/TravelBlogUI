762bac12bca89fd3707d219842ebeedd589b289b
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-09 23:25:54 +0500

    Travel Blog

--- lib/widget/most_popular.dart
+++ lib/widget/most_popular.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_blog_flutter/detail.dart';
 import 'package:travel_blog_flutter/widget/travel_blog.dart';
 
 class MostPopular extends StatelessWidget {
@@ -14,41 +15,47 @@ class MostPopular extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
         var travel = list[index];
-        return Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image.asset(
-                travel.url,
-                width: 140,
-                fit: BoxFit.cover,
-                ),
-            ),
-            Positioned(
-              bottom: 20,
-              left: 15,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Material(
-                    color: Colors.transparent,
-                    child: Text(travel.location,
-                    style: const TextStyle(color: Colors.white,
-                    fontSize: 15),
-                    ),
+        return GestureDetector(
+          onTap:(){ Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return DetailPage();
+          },));
+          },
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset(
+                  travel.url,
+                  width: 140,
+                  fit: BoxFit.cover,
                   ),
-                  Material(
-                    color: Colors.transparent,
-                    child: Text(travel.name,
-                    style: const TextStyle(color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-                    ),
-                  )
-                ],
-              )
               ),
-          ],
+              Positioned(
+                bottom: 20,
+                left: 15,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: Text(travel.location,
+                      style: const TextStyle(color: Colors.white,
+                      fontSize: 15),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: Text(travel.name,
+                      style: const TextStyle(color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                      ),
+                    )
+                  ],
+                )
+                ),
+            ],
+          ),
         );
       }, 
       separatorBuilder: (_, index) => SizedBox(width: 15 ,), 
